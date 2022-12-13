c3ca71f4d83c7a3e8a5b56873f0e7b817db8b00f
Andrei Cioboata <andrei.cioboata@gmail.com>
2022-12-13 14:05:41 +0100

    Added helper methods

--- src/document/Document.java
+++ src/document/Document.java
@@ -157,7 +157,10 @@ public abstract class Document {
 	{
 	    // TODO: You will play with this method in week 1, and 
 		// then implement it in week 2
-	    return 0.0;
+		System.out.println("Number of sentences: " + getNumSentences());
+		System.out.println("Number of words: " + getNumWords());
+		System.out.println("Number of syllables: " + getNumSyllables());
+	    return 206.835 - 1.015 * getNumWords() / getNumSentences() - 84.6 * getNumSyllables() / getNumWords();
 	}
 	
 	
